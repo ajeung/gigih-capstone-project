20cfecaf794a1c4f975d00a9b2ac24977b93da4c
DimasAfrizal <96959299+DimasAfrizal@users.noreply.github.com>
2022-06-14 17:50:27 +0700

    Add files via upload
    i upload the css variables, template header and footer also the logos

---
+++ src/footerheaderTemplate.js
@@ -0,0 +1,39 @@
+import logoHeader from './logo.png';
+import logoFooter from './logo2.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="header">
+        <div className="header-logo">
+          <a href='#' >
+            <img id='headerLogo' src={logoHeader} alt="Logo"/>
+          </a>
+        </div>
+        <div class='header-link'>
+          <a href='#'>TOKO</a>
+          <a href='#'>CARI DOKTER</a>
+          <a href='#'>ARTIKEL</a>
+          <div className="header-button">
+            <button id='loginBtn'>Login</button>
+          </div>
+        </div>
+      </div>
+      <body>
+        {/* here's write ur content */}
+      </body>
+    <div className='footer'>
+        <div className='footer-logo'>
+            <img id='footerLogo' src={logoFooter} alt="Logo"/>
+        </div>
+        <div className='footer-link'>
+          <a href='#'>Tentang Kami</a>
+          <a href='#'>Hubungi Kami</a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
