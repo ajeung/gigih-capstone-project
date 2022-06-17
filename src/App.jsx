b59bdae4103ba0db76baa364537ae60c33d2b2c3
ajeung <ajeung@student.telkomuniversity.ac.id>
2022-06-17 22:13:46 +0700

    add edit profile page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Footer from './components/header_footer/footer';
 import Header from './components/header_footer/header';
 // import DataUser from './pages/DataUser';
 import Home from './pages/Home';
+// import EditProfile from './pages/EditProfile';
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
       <body>
         <Home/>
         {/* <DataUser /> */}
+        {/* <EditProfile /> */}
       </body>
       <Footer/>
     </div>
