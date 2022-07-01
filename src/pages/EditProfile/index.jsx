039309b416eafd36b20307df1e3acc247066f0a6
ajeung <ajeung@student.telkomuniversity.ac.id>
2022-07-01 18:17:13 +0700

    move editProfile page to EditProfile folder

--- src/pages/EditProfile/index.jsx
+++ src/pages/EditProfile/index.jsx
@@ -1,3 +1,5 @@
+import photo from "../../assets/img/man.png"
+
 const EditProfile = () => {
     return (
         <div className="home__container">
@@ -5,24 +7,24 @@ const EditProfile = () => {
                 <h2>Edit Profile</h2>
                 <div className="form__card">
                     <div>
-                        <img className="profile__image" src='https://cdn-image.hipwee.com/wp-content/uploads/2018/10/hipwee-photo-1497704009475-24f74754c24e-640x427.jpg'></img>
+                        <img className="profile__image" src={photo}></img>
                     </div>
                     <div className="data__user">
                         <form>
-                            <label for="firstName" className="first-name">First Name</label>
-                            <input id="firstName" type="text"></input>
-                            <br></br>
-                            <label for="lastName" className="last-name">Last Name</label>
-                            <input id="lastName" type="text"></input>
+                            <label for="nama" className="nama">Nama</label>
+                            <input id="nama" type="text"></input>
                             <br></br>
                             <label for="ttl" className="ttl">Tanggal Lahir</label>
                             <input id="ttl" type="date"></input>
                             <br></br>
-                            <label for="email" className="email">Email</label>
-                            <input id="email" type="text"></input>
+                            <label for="jenisKelamin" className="jenisKelamin">Jenis Kelamin</label>
+                            <input id="jenisKelamin" type="text"></input>
                             <br></br>
-                            <label for="password" className="password">Password</label>
-                            <input id="password" type="password"></input>
+                            <label for="beratB" className="beratB">Berat Badan</label>
+                            <input id="beratB" type="text"></input>
+                            <br></br>
+                            <label for="tinggiB" className="tinggiB">Tinggi Badan</label>
+                            <input id="tinggiB" type="text"></input>
                             <br></br>
                             <button>Edit Profile</button>
                         </form>
