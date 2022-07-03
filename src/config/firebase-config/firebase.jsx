c06cd4cefb5fe45820fe71d92219b4bbf691932d
AdawiyyahLatifah <adawiyyahlatifah@gmail.com>
2022-07-03 22:30:29 +0700

    add env

--- src/config/firebase-config/firebase.jsx
+++ src/config/firebase-config/firebase.jsx
@@ -4,7 +4,7 @@ import 'firebase/compat/firestore'
 
 const firebaseConfig = {
     apiKey: 'AIzaSyAyl7zNqJ4MY-hcwel7jYT97rq6S020GhU',
-    authDomain: 'soluhouse2022.firebaseapp.com',
+    authDomain: process.env.AUTH_DOM,
     projectId: 'soluhouse2022',
     storageBucket: 'soluhouse2022.appspot.com',
     messagingSenderId: '1022303014861',
