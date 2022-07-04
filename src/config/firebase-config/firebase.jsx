2eab44f2fe2869ad0b6b8e761a2cc02e43a57d28
AdawiyyahLatifah <adawiyyahlatifah@gmail.com>
2022-07-04 18:19:56 +0700

    error checking

--- src/config/firebase-config/firebase.jsx
+++ src/config/firebase-config/firebase.jsx
@@ -17,4 +17,6 @@ const firebaseApp = firebase.initializeApp(firebaseConfig)
 const auth = firebase.auth()
 const provider = new firebase.auth.GoogleAuthProvider()
 
+console.log(firebaseConfig.authDomain)
+
 export { auth, provider, firebaseApp }
